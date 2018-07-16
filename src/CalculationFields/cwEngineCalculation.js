d51cb2fc9e0ba709d462f7688806c26c2bf2ec28
nevakee716 <nevakee716@gmail.com>
2018-07-16 22:55:33 +0200

    add duplicate button

--- src/CalculationFields/cwEngineCalculation.js
+++ src/CalculationFields/cwEngineCalculation.js
@@ -12,13 +12,21 @@
   };
 
   cwEngineCalculation.prototype.init = function(config) {
-    var i, o, c, operationsById = {}, allOperations = [];
-    if (!cwApi.isUndefined(config)){
-      for(i=0; i<config.length; i+=1){
+    var i, o, c, operationsById = {},
+      allOperations = [];
+    if (!cwApi.isUndefined(config)) {
+      for (i = 0; i < config.length; i += 1) {
         c = config[i];
-        o = { Id: c.Id, Label: c.Label, Order: c.Order, Description: c.Description, Type: c.Type};
+        o = {
+          Id: c.Id,
+          Label: c.Label,
+          Order: c.Order,
+          Description: c.Description,
+          Type: c.Type
+        };
+        o.config = config[i];
         o.engine = new cwApi.cwLayoutsEngine['cwEngineCalculation_' + c.Type](c);
-        if (i===0){
+        if (i === 0) {
           o.selected = true;
         }
         operationsById[c.Id] = o;
@@ -29,38 +37,40 @@
     this.Operations = allOperations;
   };
 
-  cwEngineCalculation.prototype.getConfigToSave = function($scope){
+  cwEngineCalculation.prototype.getConfigToSave = function($scope) {
     var i, json, o, res = [];
-    $scope.Operations.sort(function(a, b){return a.Order - b.Order;});
-    for (i = 0; i < $scope.Operations.length; i+=1) {
+    $scope.Operations.sort(function(a, b) {
+      return a.Order - b.Order;
+    });
+    for (i = 0; i < $scope.Operations.length; i += 1) {
       o = $scope.Operations[i];
-      if(o.engine){
+      if (o.engine) {
         json = {};
         o.engine.GetConfigurationToSave(json);
         json.Id = o.Id;
         json.Label = o.Label;
-        json.Order= o.Order;
+        json.Order = o.Order;
         json.Description = o.Description;
-        json.Type= o.Type;
+        json.Type = o.Type;
         res.push(json);
       }
     }
     return res;
   };
 
-  cwEngineCalculation.cloneObjectType = function(ot){
+  cwEngineCalculation.cloneObjectType = function(ot) {
     var ptScriptName, newOt = $.extend(true, {}, ot);
     newOt.propertiesArray = [];
-    for(ptScriptName in newOt.properties){
-      if (newOt.properties.hasOwnProperty(ptScriptName)){
+    for (ptScriptName in newOt.properties) {
+      if (newOt.properties.hasOwnProperty(ptScriptName)) {
         newOt.propertiesArray.push(newOt.properties[ptScriptName]);
       }
     }
     return newOt;
   };
 
-  cwEngineCalculation.prototype.isContentAvailable = function(){
-    if (this.Operations && this.Operations.length > 0){
+  cwEngineCalculation.prototype.isContentAvailable = function() {
+    if (this.Operations && this.Operations.length > 0) {
       return true;
     }
     return false;
@@ -68,19 +78,20 @@
 
   cwEngineCalculation.prototype.run = function($scope, config) {
     /*jslint unparam:true*/
-    var that = this, index = 0;
+    var that = this,
+      index = 0;
     $scope.Operations = this.Operations;
     $scope.OperationsById = this.OperationsById;
 
-    for(index=0; index<this.Operations.length; index+=1){
-      if (!cwApi.isUndefined(this.Operations[index].engine)){
+    for (index = 0; index < this.Operations.length; index += 1) {
+      if (!cwApi.isUndefined(this.Operations[index].engine)) {
         this.Operations[index].engine.run($scope);
       }
     }
 
     cwApi.tmp = $scope.Operations;
 
-    $scope.FilterOperators = ['Equal','NotEqual','GreaterThan','LessThan','GreaterThanEqual','LessThanEqual'/*,'In'*/];
+    $scope.FilterOperators = ['Equal', 'NotEqual', 'GreaterThan', 'LessThan', 'GreaterThanEqual', 'LessThanEqual' /*,'In'*/ ];
 
     $scope.templateByTypeOfOperation = {
       'CalculateOnSelf': that.getTemplatePath('CalculationFields', 'Template_CalculateOnSelf'),
@@ -91,18 +102,31 @@
 
     $scope.enumOperations = ['sum', 'mult', 'average', 'min', 'max', 'and', 'or', 'count'];
 
-    function getRandomId(){
+    function getRandomId() {
       var id = Math.random().toString().substring(2, 10);
-      if ($scope.OperationsById.hasOwnProperty(id)){
+      if ($scope.OperationsById.hasOwnProperty(id)) {
         return getRandomId();
       }
       return id;
     }
 
-    function getNextOrder(){
-      if ($scope.Operations.length > 0){
-        $scope.Operations.sort(function(a, b){return a.Order - b.Order;});
-        return $scope.Operations[$scope.Operations.length-1].Order + 1;
+
+    function getSelectedOperation() {
+      for (i = 0; i < $scope.Operations.length; i += 1) {
+        if ($scope.Operations[i].selected === true) {
+          return $scope.Operations[i];
+        }
+      }
+      return null;
+    };
+
+
+    function getNextOrder() {
+      if ($scope.Operations.length > 0) {
+        $scope.Operations.sort(function(a, b) {
+          return a.Order - b.Order;
+        });
+        return $scope.Operations[$scope.Operations.length - 1].Order + 1;
       }
       return 1;
     }
@@ -120,10 +144,38 @@
       $scope.displayOperation(op.Id);
     };
 
+    $scope.duplicateOperation = function(e) {
+      var selectedOp, duplicateOp;
+      e.preventDefault();
+
+
+      var selectedOp = getSelectedOperation();
+
+      if (selectedOp != null) {
+        duplicateOp = $.extend(true, {}, selectedOp);
+        duplicateOp.Id = getRandomId();
+        duplicateOp.Label += "_copy";
+        duplicateOp.Order = getNextOrder();
+      } else {
+        duplicateOp = {
+          Id: getRandomId(),
+          Label: $.i18n.prop('ec_NewOperationLabel'),
+          Order: getNextOrder()
+        };
+      }
+      duplicateOp.config.Id = duplicateOp.Id;
+      duplicateOp.engine = new cwApi.cwLayoutsEngine['cwEngineCalculation_' + duplicateOp.Type](duplicateOp.config);
+      duplicateOp.$$hashKey = undefined;
+
+      $scope.Operations.push(duplicateOp);
+      $scope.OperationsById[duplicateOp.Id] = duplicateOp;
+      $scope.displayOperation(duplicateOp.Id);
+    };
+
     $scope.displayOperation = function(selectedId) {
       var i;
-      for (i = 0; i < $scope.Operations.length; i+=1) {
-        if (selectedId === $scope.Operations[i].Id){
+      for (i = 0; i < $scope.Operations.length; i += 1) {
+        if (selectedId === $scope.Operations[i].Id) {
           $scope.Operations[i].selected = true;
         } else {
           $scope.Operations[i].selected = false;
@@ -134,36 +186,36 @@
     $scope.removeOperation = function(e, selectedId) {
       var i;
       e.preventDefault();
-      for (i = 0; i < $scope.Operations.length; i+=1) {
-        if (selectedId === $scope.Operations[i].Id){
+      for (i = 0; i < $scope.Operations.length; i += 1) {
+        if (selectedId === $scope.Operations[i].Id) {
           break;
         }
       }
       $scope.Operations.splice(i, 1);
       delete $scope.OperationsById[selectedId];
-      if ($scope.Operations.length > 0){
-        if (i === $scope.Operations.length){
-          $scope.displayOperation($scope.Operations[i-1].Id);
+      if ($scope.Operations.length > 0) {
+        if (i === $scope.Operations.length) {
+          $scope.displayOperation($scope.Operations[i - 1].Id);
         } else {
           $scope.displayOperation($scope.Operations[i].Id);
         }
       }
     };
 
-    $scope.updateOperationType = function(operation){
+    $scope.updateOperationType = function(operation) {
       operation.engine = new cwApi.cwLayoutsEngine['cwEngineCalculation_' + operation.Type]();
       operation.engine.run($scope);
     };
 
-    $scope.getPropertyDataType = function(ot, ptScriptName){
-      if (cwApi.isUndefined(ot.scriptName)){
+    $scope.getPropertyDataType = function(ot, ptScriptName) {
+      if (cwApi.isUndefined(ot.scriptName)) {
         return '';
       }
       var p = cwApi.mm.getProperty(ot.scriptName, ptScriptName);
-      if (cwApi.isUndefined(p)){
+      if (cwApi.isUndefined(p)) {
         return '';
       }
-      switch(p.type){
+      switch (p.type) {
         case 'Boolean':
           return 'checkbox';
         case 'Integer':
@@ -178,17 +230,17 @@
       }
     };
 
-    $scope.addFilter = function(evt, filters){
+    $scope.addFilter = function(evt, filters) {
       evt.preventDefault();
       filters.push({});
     };
 
-    $scope.removeFilter = function(evt, index, filters){
+    $scope.removeFilter = function(evt, index, filters) {
       evt.preventDefault();
       filters.splice(index, 1);
     };
 
-    $scope.resetFilter = function(filter){
+    $scope.resetFilter = function(filter) {
       filter.Operator = '';
       filter.Value = '';
     };
