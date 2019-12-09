80afce61da83f87b94b0c54d64b5486f836756d8
nevakee716 <nevakee716@gmail.com>
2019-12-09 12:31:47 +0100

    reformat and adding styles

--- src/CalculationFields/EngineCalculateWithIndependantList.js
+++ src/CalculationFields/EngineCalculateWithIndependantList.js
@@ -6,10 +6,11 @@
   var cwEngine, getAllAvailableObjectType, loadConfiguration;
 
   getAllAvailableObjectType = function(that) {
-    var otScriptName, allOts = cwApi.mm.getMetaModel().objectTypes;
+    var otScriptName,
+      allOts = cwApi.mm.getMetaModel().objectTypes;
     that.availableObjectTypes = {};
-    for(otScriptName in allOts){
-      if (allOts.hasOwnProperty(otScriptName) && !allOts[otScriptName].properties.hasOwnProperty('allowautomaticdeletion')){
+    for (otScriptName in allOts) {
+      if (allOts.hasOwnProperty(otScriptName) && !allOts[otScriptName].properties.hasOwnProperty("allowautomaticdeletion")) {
         that.availableObjectTypes[otScriptName] = cwApi.cwLayoutsEngine.cwEngineCalculation.cloneObjectType(allOts[otScriptName]);
       }
     }
@@ -17,33 +18,42 @@
 
   loadConfiguration = function(that, config) {
     var i, f;
-    if(!cwApi.isUndefined(config)){
+    if (!cwApi.isUndefined(config)) {
       that.Operator = config.Operation;
       that.CreateNewItem = config.CreateNew;
       // load objects
       that.Node.SelectedObjectType = that.availableObjectTypes[config.CwLightNodeObjectType.ObjectTypeScriptName];
-      
-      if (that.Node.SelectedObjectType && that.availableObjectTypes.hasOwnProperty(that.Node.SelectedObjectType.scriptName)){
+
+      if (that.Node.SelectedObjectType && that.availableObjectTypes.hasOwnProperty(that.Node.SelectedObjectType.scriptName)) {
         // filters
-        for(i=0; i<config.CwLightNodeObjectType.Filters.length; i+=1){
+        for (i = 0; i < config.CwLightNodeObjectType.Filters.length; i += 1) {
           f = config.CwLightNodeObjectType.Filters[i];
-          that.Node.Filters.push({ScriptName: f.ScriptName, Operator:f.Operator, Value: f.Value});
+          that.Node.Filters.push({ ScriptName: f.ScriptName, Operator: f.Operator, Value: f.Value });
         }
         that.Node.SelectedObjectType.Operand = that.Node.SelectedObjectType.properties[config.OperandPropertyScriptName.toLowerCase()];
       }
 
       that.ResultObjectType = that.availableObjectTypes[config.ResultObjectTypeScriptName];
-      if (that.availableObjectTypes.hasOwnProperty(that.ResultObjectType.scriptName)){
+      if (that.availableObjectTypes.hasOwnProperty(that.ResultObjectType.scriptName)) {
         that.ResultObjectType.ResultProperty = that.ResultObjectType.properties[config.ResultPropertyScriptName];
         that.ResultObjectType.IdentifierProperty = that.ResultObjectType.properties[config.IdentifierPropertyScriptName];
         that.ResultObjectType.IdentifierPropertyValue = config.IdentifierPropertyValue;
         that.CreateOptions = config.CreateOptions;
+        if (that.CreateOptions.Create === true) {
+          that.ResultObjectType.AssociationTypes.every(function(a) {
+            if (a.ScriptName === that.CreateOptions.TargetAssociationScriptName) {
+              that.ResultObjectType.ResultProperty = that.availableObjectTypes[a.TargetObjectTypeScriptName.toLowerCase()].properties[config.ResultPropertyScriptName];
+              return false;
+            }
+            return true;
+          });
+        }
         that.DefaultValues = config.DefaultValues;
       }
     }
   };
 
-  cwEngine = function(config){
+  cwEngine = function(config) {
     this.Node = {};
     this.Node.SelectedObjectType = {};
     this.Node.SelectedObjectType.Operand = {};
@@ -53,7 +63,7 @@
     this.ResultObjectType.ResultProperty = {};
     this.ResultObjectType.IdentifierProperty = {};
     this.ResultObjectType.IdentifierPropertyValue = null;
-    this.CreateNewItem = {}
+    this.CreateNewItem = {};
     this.DefaultValues = {};
 
     getAllAvailableObjectType(this);
@@ -63,55 +73,52 @@
   cwEngine.prototype.GetConfigurationToSave = function(json) {
     var f, i;
     // Important : $type must be 1st attribute
-    json.$type = 'CalculationFields.JSON.OperationNodeJsonCalculateWithIndependantList, CalculationFields';
+    json.$type = "CalculationFields.JSON.OperationNodeJsonCalculateWithIndependantList, CalculationFields";
     json.CwLightNodeObjectType = {
-      ObjectTypeScriptName : this.Node.SelectedObjectType.scriptName,
-      Properties : ['id', 'name'],
-      Filters : []
+      ObjectTypeScriptName: this.Node.SelectedObjectType.scriptName,
+      Properties: ["id", "name"],
+      Filters: [],
     };
     json.Operation = this.Operator;
     json.ResultPropertyScriptName = this.ResultObjectType.ResultProperty.scriptName;
 
     // filters
-    for(i=0; i<this.Node.Filters.length; i+=1){
+    for (i = 0; i < this.Node.Filters.length; i += 1) {
       f = this.Node.Filters[i];
-      json.CwLightNodeObjectType.Filters.push({ScriptName: f.ScriptName, Operator: f.Operator, Value: f.Value});
+      json.CwLightNodeObjectType.Filters.push({ ScriptName: f.ScriptName, Operator: f.Operator, Value: f.Value });
     }
-    
+
     // operand
     json.OperandPropertyScriptName = this.Node.SelectedObjectType.Operand.scriptName;
     json.ResultObjectTypeScriptName = this.ResultObjectType.scriptName;
     json.CreateOptions = this.CreateOptions;
-    if (!cwApi.isUndefined(this.ResultObjectType.IdentifierProperty) && !cwApi.isUndefined(this.ResultObjectType.IdentifierProperty.scriptName)){
+    if (!cwApi.isUndefined(this.ResultObjectType.IdentifierProperty) && !cwApi.isUndefined(this.ResultObjectType.IdentifierProperty.scriptName)) {
       json.IdentifierPropertyScriptName = this.ResultObjectType.IdentifierProperty.scriptName;
       json.IdentifierPropertyValue = this.ResultObjectType.IdentifierPropertyValue;
     }
-    json.DefaultValues = this.DefaultValues;  
+    json.DefaultValues = this.DefaultValues;
   };
 
-  cwEngine.prototype.run = function($scope){
+  cwEngine.prototype.run = function($scope) {
     var that = this;
-    $scope.resetPropertyIdentifier = function(){
+    $scope.resetPropertyIdentifier = function() {
       that.ResultObjectType.IdentifierProperty = {};
       that.ResultObjectType.IdentifierPropertyValue = null;
     };
 
     $scope.getAssociatedProperties = function(s) {
       var r;
-      that.ResultObjectType.AssociationTypes.forEach(function(a){
-        if(a.ScriptName === s) {
+      that.ResultObjectType.AssociationTypes.forEach(function(a) {
+        if (a.ScriptName === s) {
           r = a.TargetObjectTypeScriptName;
         }
       });
       return this.op.engine.availableObjectTypes[r.toLowerCase()].properties;
-
     };
-
   };
 
   if (cwApi.isUndefined(cwApi.cwLayoutsEngine)) {
     cwApi.cwLayoutsEngine = {};
   }
   cwApi.cwLayoutsEngine.cwEngineCalculation_CalculateWithIndependantList = cwEngine;
-
-}(cwAPI));
+})(cwAPI);
